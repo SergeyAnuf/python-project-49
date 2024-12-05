6ddea98693348c2f4a2384d022451bf4b582a86c
Sergey Anufriev <sergeysyava38@gmail.com>
2024-12-05 21:37:38 +0300

    add brain_progression.py Добавлена игра Прогрессия

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from cli_brain_progression import progression
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    progression()
+
+
+if __name__ == '__main__':
+    main()
