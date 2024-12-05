8ef6abe18aa44d74aec371017dc3f6e50541ef60
Sergey Anufriev <sergeysyava38@gmail.com>
2024-12-05 18:44:41 +0300

    add cli_brain_gcd.py

---
+++ cli_brain_gcd.py
@@ -0,0 +1,22 @@
+import prompt
+import math
+import random
+
+def gcd():
+	name = prompt.string('May I have your name? ')  
+	print(f'Hello, {name}')
+	print('Find the greatest common divisor of given numbers.')
+	i = 1
+	while i <= 3:
+		num_1 = random.randint(1, 100)
+		num_2 = random.randint(1, 100)
+		result = math.gcd(num_1, num_2)
+		answer = prompt.string(f'Question: {num_1} {num_2}: ')
+		if int(answer) == result:
+			print('Correct!')
+		else:
+			print(f"{answer} is wrong answer. Correct answer was {result}\nLet's try again, {name}")
+			break
+		i += 1
+		if i == 4:
+			print(f'Congratulation, {name}')
