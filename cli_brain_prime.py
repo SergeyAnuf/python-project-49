5c01a43c3e5faf319a1bb1d409070f342063e83b
Sergey Anufriev <sergeysyava38@gmail.com>
2024-12-06 12:16:38 +0300

    add cli_brain_prime.py

---
+++ cli_brain_prime.py
@@ -0,0 +1,27 @@
+import prompt
+import random
+
+
+def prime():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    j = 1
+    while j <= 3:
+        number = random.randint(1, 100)
+        print(f'Question: {number}')
+        if number > 1:
+            for i in range(2, (number // 2) + 1):
+                if number % i == 0:
+                    result = 'no'
+                    break
+            else:
+                result = 'yes'
+        else:
+            result = 'yes'
+        answer = prompt.string('Your answer: ')
+        if answer == result:
+            print('Correct!')
+        else:
+            print(f"{answer} is wrong answer. Correct answer was {result}\nLet's try again, {name}")
+        j += 1
